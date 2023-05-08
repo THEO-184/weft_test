5026328de1066f51e1eb4bb611340908e9e7e9dd
Theophilus Boakye <93124888+TheophilusBoakye@users.noreply.github.com>
2023-05-08 15:28:52 +0000

    remove logs

--- src/Features/Users/index.tsx
+++ src/Features/Users/index.tsx
@@ -20,65 +20,12 @@ const Users = () => {
 		<div className="w-3/4 mx-auto my-8">
 			<div className="flex flex-col">
 				<h1 className="mb-7 text-center text-black text-xl">Users Data</h1>
-				{/* <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-					<div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-						{isLoading ? (
-							"Loading..."
-						) : (
-							<div className="overflow-hidden">
-								<table
-									{...getTableProps()}
-									className="min-w-full text-left text-sm font-light"
-								>
-									<thead className="border-b font-medium dark:border-neutral-500">
-										{headerGroups.map((headerGroup) => (
-											<tr {...headerGroup.getHeaderGroupProps()}>
-												{headerGroup.headers.map((column) => (
-													<th
-														{...column.getHeaderProps()}
-														scope="col"
-														className="px-6 py-4"
-													>
-														{column.render("Header")}
-													</th>
-												))}
-											</tr>
-										))}
-									</thead>
-									<tbody {...getTableBodyProps()}>
-										{rows.map((row: any) => {
-											prepareRow(row);
-											return (
-												<tr
-													{...row.getRowProps()}
-													className="border-b dark:border-neutral-500"
-												>
-													{row.cells.map((cell: any) => {
-														return (
-															<td
-																{...cell.getCellProps()}
-																className="whitespace-nowrap px-6 py-4 font-medium"
-															>
-																{cell.render("Cell")}
-															</td>
-														);
-													})}
-												</tr>
-											);
-										})}
-									</tbody>
-								</table>
-							</div>
-						)}
-					</div>
-				</div> */}
+
 				<Pagination
 					pageCount={CustomPageCount}
 					onPageChange={handleNavigation}
 				/>
 				<Table
-					// tableColumns={columns}
-					// data={data}
 					isLoading={isLoading}
 					getTableProps={getTableProps}
 					getTableBodyProps={getTableBodyProps}
